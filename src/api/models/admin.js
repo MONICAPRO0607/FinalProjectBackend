d0fbac213d89cdd6bd320b792b54442d1a21f298
MONICAPRO0607 <monicapro0607@gmail.com>
2025-11-16 19:50:07 +0100

    Install and change bcrypt for bcryptjs

--- src/api/models/admin.js
+++ src/api/models/admin.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const AdminSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -21,7 +21,7 @@ AdminSchema.pre("save", async function (next) {
 });
 
 AdminSchema.methods.comparePassword = function (candidate) {
-  const bcrypt = require("bcrypt");
+  const bcrypt = require("bcryptjs");
   return bcrypt.compare(candidate, this.password);
 };
 
